09102ef51092b1263409aa9c910176ce1f8e45d2
Conor Kelly <L00098694@student.lyit.ie>
2021-08-12 22:06:19 +0100

    minor ios fixes

--- src/app/post-detail/post-detail.page.ts
+++ src/app/post-detail/post-detail.page.ts
@@ -49,6 +49,6 @@ export class PostDetailPage implements OnInit {
   getBackButtonText() {
     const win = window as any;
     const mode = win && win.Ionic && win.Ionic.mode;
-    return mode === 'ios' ? 'Inbox' : '';
+    return mode === 'ios' ? 'Posts' : '';
   }
 }
