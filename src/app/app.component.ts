898dc6de0304cb59cb5112086215673b90eb7efe
Conor Kelly <conormpkelly@gmail.com>
2021-08-26 00:50:50 +0100

    fix: hide player if nothing loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AudioService, AudioState } from './services/audio.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnDestroy {
+  $audio: Subscription;
+  hasCommentLoaded = false;
+
+  constructor(private audioService: AudioService) {
+    this.$audio = this.audioService
+      .getAudioSubscription()
+      .subscribe(() => {
+        this.hasCommentLoaded = true;
+      });
+  }
+
+  ngOnDestroy() {
+    this.$audio.unsubscribe();
+  }
 }
