a42486ac7852077456d76bf6d0e6210b8d959ac6
Conor Kelly <L00098694@student.lyit.ie>
2021-09-01 19:02:01 +0100

    fix: add heart favourite icon

--- src/app/shared/components/comment/comment.component.ts
+++ src/app/shared/components/comment/comment.component.ts
@@ -2,9 +2,12 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
-import { CommentReplyComponent } from '../../../pages/posts/post-detail/components/comment-reply/comment-reply.component';
-import { AudioService } from '../../../services/audio.service';
 import { Comment } from '../../../models/Comment';
+
+import { CommentReplyComponent } from '../../../pages/posts/post-detail/components/comment-reply/comment-reply.component';
+
+import { AudioService } from '../../../services/audio.service';
+import { LikesService } from 'src/app/services/likes.service';
 
 @Component({
   selector: 'app-comment',
@@ -19,6 +22,7 @@ export class CommentComponent implements OnInit {
 
   constructor(
     private audioService: AudioService,
+    private likesService: LikesService,
     private modalController: ModalController,
     private router: Router
   ) {}
@@ -30,6 +34,22 @@ export class CommentComponent implements OnInit {
   onPlay() {
     if (this.comment.recordingSrc) {
       this.audioService.play(this.comment);
+    }
+  }
+
+  async onToggleLike() {
+    const originalValue = this.comment.isLiked;
+
+    try {
+      this.comment.isLiked = !originalValue;
+
+      await this.likesService.setIsLiked({
+        commentId: this.comment.id,
+        isLiked: this.comment.isLiked,
+      });
+    } catch (err) {
+      console.log('An error occurred!');
+      this.comment.isLiked = originalValue;
     }
   }
 
